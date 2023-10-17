4302e23f56c51896ce15c471945d29937ddfc046
VU PHUOC <vdphuoc125@gmail.com>
2023-10-18 02:13:16 +0700

    aloalo

--- firmware/src/demo.cpp
+++ firmware/src/demo.cpp
@@ -1,34 +1,34 @@
-#include <WiFi.h>
-#include <SocketIoClient.h>
+// #include <WiFi.h>
+// #include <SocketIoClient.h>
 
-const char *ssid = "YourSSID";
-const char *password = "YourPassword";
-const char *serverAddress = "YourServerAddress";
+// const char *ssid = "YourSSID";
+// const char *password = "YourPassword";
+// const char *serverAddress = "YourServerAddress";
 
-SocketIoClient client;
+// SocketIoClient client;
 
-void setup()
-{
-   Serial.begin(115200);
-   delay(4000);
+// void setup()
+// {
+//    Serial.begin(115200);
+//    delay(4000);
 
-   WiFi.begin(ssid, password);
-   while (WiFi.status() != WL_CONNECTED)
-   {
-      delay(1000);
-      Serial.println("Connecting to WiFi...");
-   }
+//    WiFi.begin(ssid, password);
+//    while (WiFi.status() != WL_CONNECTED)
+//    {
+//       delay(1000);
+//       Serial.println("Connecting to WiFi...");
+//    }
 
-   Serial.println("Connected to WiFi network");
+//    Serial.println("Connected to WiFi network");
 
-   client.begin(serverAddress);
+//    client.begin(serverAddress);
 
-   // Sending data to the server without the event handler
-   client.emit("iotData", "{\"address\": \"example_address\", \"high\": 123}");
-}
+//    // Sending data to the server without the event handler
+//    client.emit("iotData", "{\"address\": \"example_address\", \"high\": 123}");
+// }
 
-void loop()
-{
-   client.loop();
-   delay(1000); // Wait for 1 second
-}
+// void loop()
+// {
+//    client.loop();
+//    delay(1000); // Wait for 1 second
+// }
